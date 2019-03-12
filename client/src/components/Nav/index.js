be1518af62833f1f4c71c3b22bea769503f6051a
gurneet33 <gurneet.me@gmail.com>
2019-03-11 21:35:12 -0400

    reacting

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/plan-meal"
+          className={window.location.pathname === "/plan-meal" ? "nav-link active" : "nav-link"}
+        >
+          Plan Your Meal
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/add-food"
+          className={window.location.pathname === "/add-food" ? "nav-link active" : "nav-link"}
+        >
+          Add New Food
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/daily-chart"
+          className={window.location.pathname === "/daily-cahrt" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
   );
 }
 
