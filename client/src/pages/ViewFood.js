9d9f576ed47e797ce00453efbb7f3fe651a0c4d5
gurneet33 <gurneet.me@gmail.com>
2019-03-17 15:36:20 -0400

    changes

--- client/src/pages/ViewFood.js
+++ client/src/pages/ViewFood.js
@@ -7,6 +7,7 @@ import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import Card from "../components/Card";
+import { FoodPic, FoodContainer } from "./FoodPic";
 
 class Food extends Component {
   state = {
@@ -15,11 +16,14 @@ class Food extends Component {
     foodName: "",
     foodGroupName: "",
     energy: "",
-    potassium: ""
+    potassium: "",
+    thumbnail: null,
+    pics: null
   };
 
   componentDidMount() {
     this.loadFoodGroupMasterAndUser("JohnSmith");
+    this.findPic(this.state.foodGroupName)
   }
 
   loadFood = () => {
@@ -35,6 +39,7 @@ class Food extends Component {
       )
       .catch(err => console.log(err));
   };
+
 
   loadFoodGroupMasterAndUser = userName => {
     API.getFoodGroupByMasterAndUser(userName)
@@ -52,11 +57,15 @@ class Food extends Component {
   };
 
   loadFoodByFoodGroupName = foodGroupName => {
+    this.findPic(foodGroupName)
+    this.setState((state, props) => {
+      return { thumbnail: state.pics }
+    })
+    console.log(this.state.thumbnail)
     API.getFoodByFoodGroupName(foodGroupName)
       .then(res =>
         this.setState({
           foodList: res.data,
-
           foodName: "",
           foodGroupName: "",
           energy: "",
@@ -89,6 +98,26 @@ class Food extends Component {
       .catch(err => console.log(err));
   };
 
+  // loading cards specific to food group on button click
+  findPic = (name) => {
+    API.getRecipes(name)
+      .then(res => {
+        const ImgRec = res.data.results.filter(recipe => Boolean(recipe.thumbnail))
+        this.setState({ pics: ImgRec.length > 0 ? ImgRec[0].thumbnail : null })
+        console.log(this.state.pics)
+      })
+      .catch(err => console.log(err));
+  };
+
+  loadFoodCards = (GroupName) => {
+    this.state.foodGroupList.map(group => {
+      if (group.foodGroupName == GroupName) {
+        this.loadFoodByFoodGroupName(GroupName)
+        console.log(GroupName, this.state.foodList, this.state.pics)
+      }
+    })
+  }
+
   render() {
     return (
       <Container fluid>
@@ -99,56 +128,78 @@ class Food extends Component {
             </Jumbotron> */}
           </Col>
         </Row>
-
-        <Row>
-          <Col size="md-12">
-            {console.log(this.state.foodGroupList)}
+        <div className="container">
+          <h4>Browse Common Foods</h4>
+          <hr />
+          <Row>
+            {/* <Col size="md-12"> */}
+            {/* {console.log(this.state.foodGroupList)} */}
             {this.state.foodGroupList.length ? (
               this.state.foodGroupList.map(foodGroupList => (
-                <Button
-                  key={foodGroupList.foodGroupName}
-                  onClick={() =>
-                    this.loadFoodByFoodGroupName(foodGroupList.foodGroupName)
-                  }
-                  className="btn btn-primary"
-                >
-                  {/* <Link to={"/food/" + food._id}></Link> */}
-                  <strong>
-                    {foodGroupList.foodGroupName} <br />
-                  </strong>
+                <Col size="lg-4">
+                  <Container>
+                    {/* {console.log(foodGroupList.pics)} */}
+                    <Row>
+                      <Col size="md-4">
+                        <img alt="foopic" src={foodGroupList.pics} />
+                      </Col>
+                      <Col size="md-8">
+                        <Button
+                          className="btn btn-default"
+                          key={foodGroupList.foodGroupName}
+                          // onClick={() =>
+                          //   this.loadFoodByFoodGroupName(foodGroupList.foodGroupName)
+                          // }
+                          loadFoodByGroupName={this.loadFoodByFoodGroupName}
+                          GroupName={foodGroupList.foodGroupName}
+                          thumbnail={foodGroupList.thumbnail}
+                          loadFoodCards={this.loadFoodCards}
+                          foodList={this.foodList}
+                        >
 
-                  {/* <DeleteBtn  /> */}
-                </Button>
+                          {/* <Link to={"/food/" + food._id}></Link> */}
+                          {/* <strong>
+                    {foodGroupList.foodGroupName} <br />
+                  </strong> */}
+
+                          {/* <DeleteBtn  /> */}
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Container>
+                </Col>
               ))
             ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
-
-        {this.state.foodList.length ? (
-          this.state.foodList.map(foodList => (
-            <Row>
-              <Col size="md-12">
-                <Button
+                <h3>No Results to Display1</h3>
+              )}
+            {/* </Col> */}
+          </Row>
+        </div>
+        <Row>
+          {this.state.foodList.length ? (
+            this.state.foodList.map(foodList => (
+              <Col size="md-3">
+                <Card
+                  foodName={foodList.foodName}
                   key={foodList._id}
                   onClick={() => this.saveFoodByUser(foodList._id, "")}
                   className="btn btn-light btn-lg btn-block"
                 >
-                  <strong>
-                    Food Name: {foodList.foodName} <br />
+
+                  {/* Food Name: {foodList.foodName} <br />
                     Food Group: {foodList.foodGroupName} <br />
                     Energy: {foodList.energy} <br />
                     Potassium: {foodList.potassium} <br />
-                    Username: {foodList.userName} <br />
-                  </strong>
-                </Button>
+                    Username: {foodList.userName} <br /> */}
+
+                </Card>
               </Col>
-            </Row>
-          ))
-        ) : (
-          <h3>No Results to Display</h3>
-        )}
+
+            ))
+          ) : (
+              <h3>No Results to Display</h3>
+            )}
+        </Row>
       </Container>
     );
   }
